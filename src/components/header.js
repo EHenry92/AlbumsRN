cc350aaed03e3028bbc794d4294b0a2bc1ebd64c
EHenry92 <EHenry692@hotmail.com>
2018-03-03 20:36:06 -0500

    header complete

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,32 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
 
-const Header = () => {
-  return <Text>Albums!</Text>;
+const styles = {
+  textStyle: {
+    fontSize: 20
+  },
+  viewStyle: {
+    backgroundColor: 'green',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: 'black',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
+  }
 };
+
+const Header = (props) => {
+  const {textStyle, viewStyle} = styles;
+return (
+  <View style={viewStyle}>
+    <Text style={textStyle}>{props.title}</Text>
+  </View>
+);
+};
+
+
 export default Header;
