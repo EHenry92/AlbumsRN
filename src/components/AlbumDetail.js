64b5249a8f7d2117ee57bdfe45ee93a646be2701
EHenry92 <EHenry692@hotmail.com>
2018-03-06 07:14:01 -0500

    artwork shows

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -5,20 +5,23 @@ import CardSection from './CardSection';
 
 
 const AlbumDetail = ({album}) => {
-  const {thumbnail_image, title, artist} = album;
-  const {imageStyle, headerContentStyle} = styles;
+  const {thumbnail_image, title, artist, image} = album;
+  const {artistImageStyle, headerContentStyle, imageContainerStyle, albumTitleStyle, albumImageStyle} = styles;
   return (
   <Card>
     <CardSection>
-      <View>
-        <Image style={imageStyle} source ={{uri: thumbnail_image}} />
+      <View style={imageContainerStyle}>
+        <Image style={artistImageStyle} source ={{uri: thumbnail_image}} />
       </View>
       <View style ={headerContentStyle}>
-      <Text>{title}</Text>
+      <Text style = {albumTitleStyle}>{title}</Text>
       <Text>{artist}</Text>
       </View>
     </CardSection>
-  </Card>)
+    <CardSection>
+      <Image style={albumImageStyle} source={{uri: image}} />
+    </CardSection>
+  </Card>);
 
 };
 export default AlbumDetail;
@@ -28,8 +31,22 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around'
   },
-  imageStyle: {
+  artistImageStyle: {
     height: 50,
-    width: 50
+    width: 50,
+  },
+  imageContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  albumTitleStyle: {
+    fontSize: 18
+  },
+  albumImageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 };
